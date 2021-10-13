9f68fd53a9f93f8a30a39c4f4f5329a901c40f93
Rastamannn <rstalman@gmail.com>
2021-10-13 21:04:08 +0200

    05_03

--- 05_howler/howler.py
+++ 05_howler/howler.py
@@ -2,10 +2,11 @@
 """
 Author : Anonymous <Anonymous@localhost>
 Date   : 2021-10-11
-Purpose: Rock the Casbah
+Purpose: Howler (Upper cases input)
 """
 
 import argparse
+import os
 
 
 # --------------------------------------------------
@@ -13,38 +14,19 @@ def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='Rock the Casbah',
+        description='Howler (upper cases input)',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
-    parser.add_argument('positional',
-                        metavar='str',
-                        help='A positional argument')
-
-    parser.add_argument('-a',
-                        '--arg',
-                        help='A named string argument',
-                        metavar='str',
-                        type=str,
-                        default='')
-
-    parser.add_argument('-i',
-                        '--int',
-                        help='A named integer argument',
-                        metavar='int',
-                        type=int,
-                        default=0)
-
-    parser.add_argument('-f',
-                        '--file',
-                        help='A readable file',
-                        metavar='FILE',
-                        type=argparse.FileType('rt'),
-                        default=None)
+    parser.add_argument('text',
+                        metavar='text',
+                        help='Input string or file')
 
     parser.add_argument('-o',
-                        '--on',
-                        help='A boolean flag',
-                        action='store_true')
+                        '--outfile',
+                        help='Output filename',
+                        metavar='outfile',
+                        type=str,
+                        default='')
 
     return parser.parse_args()
 
@@ -54,17 +36,20 @@ def main():
     """Make a jazz noise here"""
 
     args = get_args()
-    str_arg = args.arg
-    int_arg = args.int
-    file_arg = args.file
-    flag_arg = args.on
-    pos_arg = args.positional
-
-    print(f'str_arg = "{str_arg}"')
-    print(f'int_arg = "{int_arg}"')
-    print('file_arg = "{}"'.format(file_arg.name if file_arg else ''))
-    print(f'flag_arg = "{flag_arg}"')
-    print(f'positional = "{pos_arg}"')
+    input_str = args.text
+    text=''
+    if os.path.isfile(input_str):
+        in_fh = open(input_str)
+        text = in_fh.read()
+    else:
+        text = input_str
+    output_str = text.upper().rstrip()
+    
+    if args.outfile == '':
+        print(output_str)
+    else:
+        out_fh = open(args.outfile, 'wt')
+        out_fh.write(output_str)
 
 
 # --------------------------------------------------
